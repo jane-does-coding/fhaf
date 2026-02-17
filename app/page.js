e2a1147b3b7389fe4da54a344c5c174b61dff226
Jane <yevheniiasimaka@gmail.com>
2026-02-17 14:53:35 -0700

    added gradient and glowing to monitors

--- app/page.js
+++ app/page.js
@@ -52,37 +52,42 @@ export default function Home() {
 
 	return (
 		<div className="relative w-full h-screen overflow-hidden bg-black">
-			{/* MAIN BACKGROUND (after start) */}
+			{/* Main Background - Monitors (after start) */}
 			<img
-				src="img5.png"
+				src="/imgs/main-bg-monitors.png"
 				className={`absolute inset-0 z-[1] w-[100vw] h-[90vh] object-fill transition-opacity duration-700  ${
 					started ? "opacity-100" : "opacity-0"
 				}`}
 			/>
+			<div
+				className={`absolute inset-0 z-[1] w-[100vw] h-[20vh] top-[80vh] object-fill transition-opacity duration-700 bg-linear-to-b from-10% from-black/70 to-black  ${
+					started ? "opacity-100" : "opacity-0"
+				}`}
+			></div>
 
-			{/* SIDE MONITOR - NOW ACTS AS MENU PANEL */}
+			{/* Side Monitor Container */}
 			<div
 				className={`absolute w-[70vw] h-[70vh] left-[30vw] top-[50vh] translate-y-[-50%] transition-all duration-600 ease-in-out z-99 ${
 					started ? "opacity-100" : "opacity-0"
 				} ${menuVisible ? "translate-x-0" : "translate-x-full"}`}
 			>
-				{/* Monitor Background Image */}
+				{/* Side Monitor Image */}
 				<img
 					src="side-monitor.webp"
-					className="w-full h-full object-fill absolute inset-0 drop-shadow-2xl drop-shadow-neutral-540/40"
+					className="w-full h-full object-fill absolute inset-0 drop-shadow-2xl drop-shadow-neutral-400/40"
 				/>
 
 				{/* Close Button */}
 				{menuVisible && (
 					<button
 						onClick={handleCloseMenu}
-						className="absolute top-[5vh] right-[6vw] text-white text-[7vh] cursor-pointer transition-all z-[101] choco"
+						className="absolute top-[6vh] right-[6vw] text-white text-[7vh] cursor-pointer transition-all z-[101] choco"
 					>
 						X
 					</button>
 				)}
 
-				{/* FAQ Content - shown when faq menu is active */}
+				{/* FAQ Content */}
 				{activeMenu === "faq" && menuVisible && (
 					<div className="absolute inset-0 px-[2vw] pt-[1vh] py-[3vh] bg-yellow-200/0 w-[55vw] h-[50vh] top-[10vh] left-[8vw] text-white overflow-y-auto z-[100]">
 						<h2 className="text-[7vh] leading-[7vh] choco font-bold mb-6 text-white mt-8">
@@ -117,7 +122,7 @@ export default function Home() {
 					</div>
 				)}
 
-				{/* Learn More Content - shown when learn menu is active */}
+				{/* Learn More Content */}
 				{activeMenu === "learn" && menuVisible && (
 					<div className="absolute inset-0 px-[2vw] pt-[1vh] py-[3vh] bg-yellow-200/0 w-[55vw] h-[50vh] top-[10vh] left-[8vw] text-white overflow-y-auto z-[100]">
 						<h2 className="text-[7vh] leading-[7vh] choco font-bold mb-6 text-white mt-8">
@@ -143,38 +148,54 @@ export default function Home() {
 				)}
 			</div>
 
-			{/* AUDIO */}
-			<audio
+			{/* Audio */}
+			{/* 	<audio
 				ref={audioRef}
 				src="/sounds/forbidden-nocturne.mp3"
 				loop
 				playsInline
 				preload="auto"
-			/>
+			/> */}
 			<audio ref={clickRef} src="/sounds/mouse-click.mp3" preload="auto" />
 
-			{/* SCREEN OVERLAY */}
+			{/* Monitor Screens */}
 			{started && (
 				<div className="z-80">
-					<div onClick={() => handleMenuClick("learn")} className="">
+					<div onClick={() => handleMenuClick("learn")} className="group">
+						<img
+							src="/imgs/learn-more.png"
+							className="z-80 absolute right-[18.5vw] w-[28.5vw] h-[45vh] top-[18.5vh] hover:scale-[103%] transition-all ease-in-out cursor-pointer group-hover:blur-xl"
+						/>
 						<img
 							src="/imgs/learn-more.png"
 							className="z-80 absolute right-[18.5vw] w-[28.5vw] h-[45vh] top-[18.5vh] hover:scale-[103%] transition-all ease-in-out cursor-pointer"
 						/>
 					</div>
-					<div onClick={playClick} className="">
+					<div onClick={playClick} className="group">
+						<img
+							src="/imgs/submit.png"
+							className="z-80 absolute left-[29.75vw] w-[10.5vw] h-[13.5vh] top-[52vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer group-hover:blur-lg"
+						/>
 						<img
 							src="/imgs/submit.png"
 							className="z-80 absolute left-[29.75vw] w-[10.5vw] h-[13.5vh] top-[52vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer"
 						/>
 					</div>
-					<div onClick={() => handleMenuClick("faq")} className="">
+					<div onClick={() => handleMenuClick("faq")} className="group">
+						<img
+							src="/imgs/faq.png"
+							className="z-80 absolute left-[29.5vw] w-[10.25vw] h-[19.5vh] top-[26vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer group-hover:blur-lg"
+						/>
 						<img
 							src="/imgs/faq.png"
 							className="z-80 absolute left-[29.5vw] w-[10.25vw] h-[19.5vh] top-[26vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer"
 						/>
 					</div>
-					<div onClick={playClick} className="">
+					<div onClick={playClick} className="group">
+						<img
+							src="/imgs/slack.png"
+							className="z-80 absolute left-[40.5vw] w-[10.75vw] h-[26vh] top-[47.25vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer group-hover:blur-lg"
+						/>
 						<img
 							src="/imgs/slack.png"
 							className="z-80 absolute left-[40.5vw] w-[10.75vw] h-[26vh] top-[47.25vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer"
@@ -190,7 +211,7 @@ export default function Home() {
 				</div>
 			)}
 
-			{/* START SCREEN OVERLAY */}
+			{/* Intro Screen */}
 			{!started && (
 				<div className="absolute inset-0 z-10 flex items-center justify-center">
 					{/* background image */}
