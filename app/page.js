c33871651333f6fa8518857a44576575360fc930
Jane <yevheniiasimaka@gmail.com>
2026-02-17 09:37:15 -0700

    positioning of monitors

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 export default function Home() {
 	const audioRef = useRef(null);
 	const clickRef = useRef(null);
-	const [started, setStarted] = useState(false);
+	const [started, setStarted] = useState(true);
 
 	const handleStart = () => {
 		if (audioRef.current) {
@@ -21,7 +21,6 @@ export default function Home() {
 		clickRef.current.play();
 	};
 
-	// Prepare audio silently (optional but smooth)
 	useEffect(() => {
 		if (audioRef.current) {
 			audioRef.current.muted = true;
@@ -30,11 +29,18 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className="relative w-full h-screen overflow-hidden">
+		<div className="relative w-full h-screen overflow-hidden bg-black">
 			{/* MAIN BACKGROUND (after start) */}
 			<img
 				src="img5.png"
-				className={`absolute inset-0 z-[1] w-full h-full object-cover transition-opacity duration-700 ${
+				className={`absolute inset-0 z-[1] w-[100vw] h-[90vh] object-fill transition-opacity duration-700  ${
+					started ? "opacity-100" : "opacity-0"
+				}`}
+			/>
+
+			<img
+				src="monitor.webp"
+				className={`absolute inset-0 w-[85vw] h-[90vh] left-[7.5vw] top-[10vh] object-fill transition-opacity duration-700 z-99  ${
 					started ? "opacity-100" : "opacity-0"
 				}`}
 			/>
@@ -50,11 +56,36 @@ export default function Home() {
 
 			{/* SCREEN OVERLAY */}
 			{started && (
-				<div className="z-99">
-					<div
-						onClick={playClick}
-						className="w-[8vw] h-[13vh] bg-yellow-200 absolute top-[32.5vh] left-0 z-99"
-					></div>
+				<div className="z-80">
+					<div onClick={playClick} className="">
+						<img
+							src="/imgs/learn-more.png"
+							className="z-80 absolute right-[18.5vw] w-[28.5vw] h-[45vh] top-[18.5vh] hover:scale-[103%] transition-all ease-in-out cursor-pointer"
+						/>
+					</div>
+					<div onClick={playClick} className="">
+						<img
+							src="/imgs/submit.png"
+							className="z-80 absolute left-[29.75vw] w-[10.5vw] h-[13.5vh] top-[52vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer"
+						/>
+					</div>
+					<div onClick={playClick} className="">
+						<img
+							src="/imgs/faq.png"
+							className="z-80 absolute left-[29.5vw] w-[10.25vw] h-[19.5vh] top-[26vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer"
+						/>
+					</div>
+					<div onClick={playClick} className="">
+						<img
+							src="/imgs/slack.png"
+							className="z-80 absolute left-[40.5vw] w-[10.75vw] h-[26vh] top-[47.25vh] hover:scale-[104%] transition-all ease-in-out cursor-pointer"
+						/>
+					</div>
+					{/* 	<div className="w-[20vw] h-fit border-green-300/0 border-4 flex items-center justify-center absolute z-99 top-[34vh] right-[21%] rotate-[-4deg]">
+						<h3 className="choco text-center font-semibold text-[12vh] leading-[11.5vh]">
+							Learn More
+						</h3>
+					</div> */}
 					<h1 className="heref text-[10vh] leading-[10vh] text-white absolute top-[2vh] left-[2vw] z-99">
 						Five Nights <br /> at Freddy's
 					</h1>
